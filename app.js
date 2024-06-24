aa2a60fac2418d525e5abb0a7341c85304cd0952
A7madSwalhi <ahmad.alsawalhi10@gmail.com>
2024-06-24 21:03:19 +0300

    The weather app

---
+++ app.js
@@ -0,0 +1,49 @@
+const select = document.querySelector(".location-button");
+const loc = document.querySelector(".location");
+const pressure = document.querySelector(".PRESSURE");
+const humidity = document.querySelector(".humidity-view");
+const wind = document.querySelector(".wind-view");
+const temp = document.querySelector(".weather-temp");
+const weades = document.querySelector(".weather-desc");
+
+const changeWhe = function (lat, long) {
+    const wether = new XMLHttpRequest();
+    wether.open(
+        "GET",
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=04c2da5ffc8ec18ecedd45d8daba00b7&units=metric`
+    );
+    wether.send();
+
+    wether.onload = () => {
+        console.log(wether.responseText);
+        const data = JSON.parse(wether.responseText);
+        loc.textContent = `${select.value}, ${data.sys.country}`;
+        console.log(data.main.pressure);
+        console.log(data.main.humidity);
+        console.log(data.wind.speed);
+        console.log(data);
+        humidity.textContent = `${data.main.humidity} %`;
+        pressure.textContent = `${data.main.pressure} %`;
+        wind.textContent = `${data.wind.speed} km/h`;
+        temp.textContent = `${Math.round(data.main.temp)}°C`;
+        weades.textContent = data.weather[0].description;
+    };
+};
+
+select.addEventListener("change", () => {
+    const lat = [31.943584, 48.828057, 51.477904];
+    const long = [35.90591, 2.34519, -0.112312];
+
+    if (select.value == "Amman") {
+        lat1 = lat[0];
+        long1 = long[0];
+    } else if (select.value == "Paris") {
+        lat1 = lat[1];
+        long1 = long[1];
+    } else if (select.value == "London") {
+        lat1 = lat[2];
+        long1 = long[2];
+    }
+
+    changeWhe(lat1, long1);
+});
